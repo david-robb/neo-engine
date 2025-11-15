a0f916e3613d2e325f6261f834078cceda2144b1
David Robbins <robbi183@msu.edu>
2025-11-15 15:28:57 -0500

    fix angle units

--- src/orbit.ts
+++ src/orbit.ts
@@ -1,81 +1,90 @@
 import { Asteroid } from './asteroid';
 import { Vector3 } from 'three';
+import { toRad } from './util';
 
 export function calculateOrbit(asteroid: Asteroid): Vector3[] {
-  let perihelionAngle: number = 0.0;
+    let perihelionAngle: number = 0.0;
 
-  const segments = 5000;
-  const accumulator = 360.0 / segments;
+    const segments = 3000;
+    const accumulator = 360.0 / segments;
 
-  const points: Vector3[] = [];
-  for (let i = 0; i < segments; i++) {
-    const radius = calculateRadius(asteroid, perihelionAngle);
+    const points: Vector3[] = [];
+    for (let i = 0; i < segments; i++) {
+        const orbitalCoordinates = toOrbitalCoordinates(asteroid, perihelionAngle);
+        const eclipticCoordinates = toEclipticCoordinates(asteroid, orbitalCoordinates);
 
-    const xOrb = radius * Math.cos(perihelionAngle);
-    const yOrb = radius * Math.sin(perihelionAngle);
+        points.push(eclipticCoordinates);
 
-    const x = transformX(asteroid, xOrb, yOrb);
-    const y = transformY(asteroid, xOrb, yOrb);
-    const z = transformZ(asteroid, xOrb, yOrb);
+        perihelionAngle += accumulator;
+    }
 
-    points.push(new Vector3(x, y, z));
+    return points;
+}
 
-    perihelionAngle += accumulator;
-  }
+function toOrbitalCoordinates(asteroid: Asteroid, trueAnomaly: number): Vector3 {
+    const radius = calculateRadius(asteroid, trueAnomaly);
 
-  console.log(points);
+    const xOrb = radius * Math.cos(trueAnomaly);
+    const yOrb = radius * Math.sin(trueAnomaly);
 
-  return points;
+    return new Vector3(xOrb, yOrb, 0);
 }
 
 function calculateRadius(asteroid: Asteroid, perihelionAngle: number): number {
-  return (
-    (+asteroid.orbital_data?.semi_major_axis! *
-      (1.0 - Math.pow(+asteroid.orbital_data?.eccentricity!, 2))) /
-    (1.0 + +asteroid.orbital_data?.eccentricity! * Math.cos(perihelionAngle))
-  );
+    return (
+        (+asteroid.orbital_data?.semi_major_axis! * (1.0 - Math.pow(+asteroid.orbital_data?.eccentricity!, 2))) /
+        (1.0 + +asteroid.orbital_data?.eccentricity! * Math.cos(perihelionAngle))
+    );
 }
 
-function transformX(asteroid: Asteroid, x: number, y: number) {
-  const inclination = +asteroid.orbital_data?.inclination!;
-  const longAscendingNode = +asteroid.orbital_data?.ascending_node_longitude!;
-  const argPerihelion = +asteroid.orbital_data?.perihelion_argument!;
-
-  const p1 =
-    x *
-    (Math.cos(longAscendingNode) * Math.cos(argPerihelion) -
-      Math.sin(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
-  const p2 =
-    y *
-    (Math.sin(longAscendingNode) * Math.cos(argPerihelion) +
-      Math.cos(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
-
-  return p1 - p2;
+function toEclipticCoordinates(asteroid: Asteroid, orbitalCoordinates: Vector3): Vector3 {
+    return new Vector3(
+        transformX(asteroid, orbitalCoordinates),
+        transformY(asteroid, orbitalCoordinates),
+        transformZ(asteroid, orbitalCoordinates)
+    );
 }
 
-function transformY(asteroid: Asteroid, x: number, y: number) {
-  const inclination = +asteroid.orbital_data?.inclination!;
-  const longAscendingNode = +asteroid.orbital_data?.ascending_node_longitude!;
-  const argPerihelion = +asteroid.orbital_data?.perihelion_argument!;
+function transformX(asteroid: Asteroid, orbitalCoordinates: Vector3) {
+    const inclination = +asteroid.orbital_data?.inclination!;
+    const longAscendingNode = toRad(+asteroid.orbital_data?.ascending_node_longitude!);
+    const argPerihelion = toRad(+asteroid.orbital_data?.perihelion_argument!);
 
-  const p1 =
-    x *
-    (Math.sin(longAscendingNode) * Math.cos(argPerihelion) +
-      Math.cos(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
-  const p2 =
-    y *
-    (Math.cos(longAscendingNode) * Math.cos(argPerihelion) -
-      Math.sin(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
+    const p1 =
+        orbitalCoordinates.x *
+        (Math.cos(longAscendingNode) * Math.cos(argPerihelion) -
+            Math.sin(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
+    const p2 =
+        orbitalCoordinates.y *
+        (Math.sin(longAscendingNode) * Math.cos(argPerihelion) +
+            Math.cos(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
 
-  return p1 + p2;
+    return p1 - p2;
 }
 
-function transformZ(asteroid: Asteroid, x: number, y: number) {
-  const inclination = +asteroid.orbital_data?.inclination!;
-  const argPerihelion = +asteroid.orbital_data?.perihelion_argument!;
+function transformY(asteroid: Asteroid, orbitalCoordinates: Vector3) {
+    const inclination = +asteroid.orbital_data?.inclination!;
+    const longAscendingNode = toRad(+asteroid.orbital_data?.ascending_node_longitude!);
+    const argPerihelion = toRad(+asteroid.orbital_data?.perihelion_argument!);
 
-  return (
-    x * (Math.sin(argPerihelion) * Math.sin(inclination)) +
-    y * (Math.cos(argPerihelion) * Math.sin(inclination))
-  );
+    const p1 =
+        orbitalCoordinates.x *
+        (Math.sin(longAscendingNode) * Math.cos(argPerihelion) +
+            Math.cos(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
+    const p2 =
+        orbitalCoordinates.y *
+        (Math.cos(longAscendingNode) * Math.cos(argPerihelion) -
+            Math.sin(longAscendingNode) * Math.sin(argPerihelion) * Math.cos(inclination));
+
+    return p1 + p2;
+}
+
+function transformZ(asteroid: Asteroid, orbitalCoordinates: Vector3) {
+    const inclination = +asteroid.orbital_data?.inclination!;
+    const argPerihelion = toRad(+asteroid.orbital_data?.perihelion_argument!);
+
+    return (
+        orbitalCoordinates.x * (Math.sin(argPerihelion) * Math.sin(inclination)) +
+        orbitalCoordinates.y * (Math.cos(argPerihelion) * Math.sin(inclination))
+    );
 }
